a37d9dc2ddd00d1dbf30a8e205b44146a2de64bc
andrewSaver <andysaver@gmail.com>
2018-01-12 15:27:45 -0500

    slick slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 document.getElementById("header").onclick = function () {
     window.location = "index.html";
 }
+
+$(document).ready(function () {
+    $('.HQ_carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 1200,
+        fade: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+       
+    });
+});
+

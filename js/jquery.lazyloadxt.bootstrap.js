7cd477b320550a1eced76e1eaa989277ed8c0fca
andrewSaver <andysaver@gmail.com>
2018-01-16 16:48:38 -0500

    lazy load test

---
+++ js/jquery.lazyloadxt.bootstrap.js
@@ -0,0 +1,3 @@
+(function ($) {
+    $.lazyLoadXT.updateEvent += ' shown.bs.modal shown.bs.dropdown shown.bs.tab shown.bs.tooltip shown.bs.popover shown.bs.collapse slid.bs.carousel';
+})(window.jQuery || window.Zepto || window.$);

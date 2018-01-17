6e5b60b388d1de0ee544b68cfabc8899455afc65
andrewSaver <andysaver@gmail.com>
2018-01-16 19:07:51 -0500

    animation and fade in

--- js/jquery.lazyloadxt.bootstrap.js
+++
@@ -1,3 +0,0 @@
-(function ($) {
-    $.lazyLoadXT.updateEvent += ' shown.bs.modal shown.bs.dropdown shown.bs.tab shown.bs.tooltip shown.bs.popover shown.bs.collapse slid.bs.carousel';
-})(window.jQuery || window.Zepto || window.$);

5ff52b5bcde9a2a3ffcda349bccae7723d596d93
andrewSaver <andysaver@gmail.com>
2018-01-22 16:23:13 -0500

    test

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,26 @@ document.getElementById('header').onclick = function () {
     window.location = 'index.html';
 }
 
+//Caption fade-in animate.css
 
 $('figure-caption').addClass('animated fadeInUp');
 
 
+//Return-to-top function
 
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 300) {
+        $('#return-to-top').fadeIn(200);
+
+    } else {
+        $('#return-to-top').fadeOut(200);
+    }
+});
+
+$('#return-to-top').click(function () {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 600); 
+});
     
 

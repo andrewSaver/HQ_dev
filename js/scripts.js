271411f2c44101e484a523949689036e6a254690
andrewSaver <andysaver@gmail.com>
2018-01-23 14:09:47 -0500

    test loading icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+// Wait for page to load
+
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('home-page', true);
+    show('loading', false);
+});
+
 //Header link to index
 
 document.getElementById('header').onclick = function () {
@@ -28,7 +50,9 @@ $('figure-caption').addClass('animated fadeInUp');
             return false;
         });
         
-    });
+});
+    
+// Floating email UI
 
 $(document).ready(function () {
     $(window).scroll(function () {
